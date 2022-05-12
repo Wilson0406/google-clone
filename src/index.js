51d34dbfad3495da0664be0879f4f6f789ed4cce
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-05-12 22:25:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import reducer, { initialState } from "./reducer";
+import { StateProvider } from "./StateProvider";
 
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
