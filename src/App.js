e86c17cc372e58308bc35d7ac1345be19bc038ab
Wilson Vidyut Doloy <54664361+Wilson0406@users.noreply.github.com>
2022-05-12 22:33:44 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './App.css';
+import Home from "./Home";
+import SearchPage from "./SearchPage";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+function App() {
+  return (
+    //
+    <div className="app">
+      <Router>
+        
+        <Switch>
+          <Route path="/search">
+                <SearchPage />
+          </Route>
+          <Route path="/">
+              <Home />
+          </Route>
+
+        </Switch>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
